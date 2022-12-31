fce9ed8c9e9c23a94c0ec3feb459a173064a4bff
mjhillman <mike@mikehillman.net>
2022-12-31 15:19:12 -0500

    Removed unneccessary files

---
+++ HITS.Component.OpenAI/OpenAIInterface.razor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+
+namespace HITS.Component.OpenAI
+{
+    public partial class OpenAIInterface: ComponentBase
+    {
+        private string _response = "";
+        private bool _processing = false;
+        private OpenAIRequest _openAIRequest;
+
+        [Parameter]
+        public string ApiKey { get; set; }
+
+        [Parameter]
+        public string OrgId { get; set; }
+
+        [Parameter]
+        public bool ShowWarning { get; set; } = true;
+
+        [Parameter]
+        public bool ShowOptions { get; set; } = true;
+
+        protected override void OnInitialized()
+        {
+            _openAIRequest = new OpenAIRequest();
+            _openAIRequest.Prompt = "Who was Thomas Paine?";
+            _openAIRequest.Temperature = 0;
+            _openAIRequest.Model = "text-davinci-003";
+            _openAIRequest.MaxTokens = 256;
+        }
+
+        private async Task OnSubmitAsync()
+        {
+            try
+            {
+                _response = "";
+                _processing = true;
+                await Task.Delay(100);
+
+                if (!string.IsNullOrWhiteSpace(_openAIRequest.Prompt))
+                {
+                    using (OpenAI openAI = new OpenAI(ApiKey, OrgId))
+                    {
+                        _response = openAI.CallOpenAI(_openAIRequest);
+                        _openAIRequest.Prompt = "";
+                    }
+                }
+                else
+                {
+                    _response = "Please enter some prompt text.";
+                }
+
+                _processing = false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
